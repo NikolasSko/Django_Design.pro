50732611d876cb2843e1b89c9a31fd510fd11f73
NikolasSko <Skobelev.nikita12345@mail.ru>
2023-11-13 11:43:26 +0700

    Создание модели заявок и главной страницы

--- designPro/design_studio/models.py
+++ designPro/design_studio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Request(models.Model):
+    CATEGORY_CATEGORY = [
+        ('house', 'жилой_дом'),
+        ('building', 'здание'),
+        ('not-stated', 'не известно')
+    ]
+    category = models.CharField(max_length=254, verbose_name='Статус', choices=CATEGORY_CATEGORY, default='not-stated')
+    name = models.CharField('Название заявки', max_length=100)
+    description = models.TextField('Описание заявки')
+    image = models.ImageField('Изображение помещения', upload_to='image/%Y')
+    CATEGORY_STATUS = [
+        ('New', 'новая'),
+        ('Done', 'выполненная'),
+        ('employed', 'принятая в работу')
+    ]
+    status = models.CharField(max_length=254, verbose_name='Статус', choices=CATEGORY_STATUS, default='New')
+    date = models.DateField('Дата публикации заявки')
+
+    def __str__(self):
+        return f'{self.name} {self.date}'
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
--- designPro/design_studio/views.py
+++ designPro/design_studio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Request
 
-# Create your views here.
+class Index(View):
+    def get(self, request):
+        requests = Request.objects.all().order_by('-date')[:4]
+        return render(request, 'index.html', {'request_list': requests})
