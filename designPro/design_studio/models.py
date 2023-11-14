2262c2308f90f66d75966e545baffb6c46a7e4a2
Nikita <Skobelev.nikita12345@mail.ru>
2023-11-14 14:17:22 +0700

    Вывод заявок на главную страницу

--- designPro/design_studio/models.py
+++ designPro/design_studio/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth import get_user_model
 
 class Request(models.Model):
     CATEGORY_CATEGORY = [
-        ('house', 'жилой_дом'),
-        ('building', 'здание'),
+        ('2D', '2D-дизайн'),
+        ('3D', '3D-дизайн'),
         ('not-stated', 'не известно')
     ]
-    category = models.CharField(max_length=254, verbose_name='Статус', choices=CATEGORY_CATEGORY, default='not-stated')
+    category = models.CharField(max_length=254, verbose_name='Категория', choices=CATEGORY_CATEGORY, default='not-stated')
     name = models.CharField('Название заявки', max_length=100)
     description = models.TextField('Описание заявки')
     image = models.ImageField('Изображение помещения', upload_to='image/%Y')
@@ -17,7 +17,7 @@ class Request(models.Model):
         ('employed', 'принятая в работу')
     ]
     status = models.CharField(max_length=254, verbose_name='Статус', choices=CATEGORY_STATUS, default='New')
-    date = models.DateField('Дата публикации заявки')
+    date = models.DateTimeField('Дата публикации')
 
     def __str__(self):
         return f'{self.name} {self.date}'
